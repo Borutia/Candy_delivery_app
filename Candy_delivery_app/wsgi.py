d64bb26dc28fa93a1c852ec080c9884ea3b40cda
Dmitry Borutya <borutia@mail.ru>
2021-03-10 15:05:20 +0500

    rename project

--- Candy_delivery_app/wsgi.py
+++ Candy_delivery_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Candy_delivery_online_store project.
+WSGI config for Candy_delivery_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Candy_delivery_online_store.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Candy_delivery_app.settings')
 
 application = get_wsgi_application()
